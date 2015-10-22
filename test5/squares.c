6a9c874aa8f38c0ed816f55e796992751c3c69cf
F10 <mr.greyforever@gmail.com>
2015-10-22 09:10:52 +0800

    solve squares using preprocessing

--- test5/squares.c
+++ test5/squares.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #define MaxLength 2000
-int i;
+/*int i;
 int N=0;
 int M;
-bool paintboard[MaxLength + 1][MaxLength + 1];
+bool board[MaxLength + 1][MaxLength + 1];
 void paint(int k,int j);
 int main(){
 	FILE *in,*out;
@@ -39,4 +40,66 @@ void paint(int k,int j){
 	for(; n >= n_boundary ; n--)
 		for(m = j; m >= m_boundary ; m--)
 			paintboard[n][m]=true;
+}*/
+#define cpt_obstacle(x) (((x) == '0') ? 1 : 0)
+char board[MaxLength + 1][MaxLength + 1];
+int **obstacle;
+int i;
+int N=1;
+int M;
+void compute_obstacle(int i); //obstacle[*][0]  = 0
+int main(){
+	FILE *in,*out;
+	in = fopen("squares.in","r");
+	out = fopen("squares.out","w");
+	fscanf(in,"%d",&i);
+	char line[MaxLength + 1];
+	fscanf(in,"%s\n",line+1);
+	M = strlen(line+1);	
+	int j,k;
+	for(j = 1 ; j <= M ; j++)
+			board[1][j] = line[j];
+	for(j = 1 ; j <= M ; j++)
+			board[0][j] = '1';
+	while(fgets(line+1,MaxLength,in) != NULL){
+		N++;
+		for(j = 1 ; j <= M ; j++)
+			board[N][j] = line[j];
+	}
+	obstacle = malloc(sizeof(int *) * (N-i + 2)); 
+	compute_obstacle(i);
+	/*for(j = 1; j <= N-i+1 ; j++){
+		for(k = 1 ; k <= M ; k++)
+			printf("%d ",obstacle[j][k]);
+		printf("\n");
+	}*/
+	int feasible = 0;
+	for(j = 1 ; j <= N - i + 1 ; j++){
+		int total_obstacle = 0;
+		for(k = 1 ; k < i ; k++)
+			total_obstacle += obstacle[j][k];
+		for(k = 1 ; k <= M - i + 1 ; k++){
+			total_obstacle += (obstacle[j][i+k-1] - obstacle[j][k-1]);
+			if(total_obstacle == 0)
+				feasible++;
+		}
+	}
+	fprintf(out,"%d\n",feasible);
+	return 0;
+}
+void compute_obstacle(int i)
+{
+	int j=0,k;
+	for(;j <= N-i+1 ; j++)		
+		obstacle[j] = calloc(M + 1 , sizeof(int));
+	for(j=1 ; j <= M ; j++){
+		int total_obstacle = 0;
+		for(k = 1 ; k < i ; k++)
+			total_obstacle += cpt_obstacle(board[k][j]);
+		for(k = 1 ; k <= N-i+1 ; k++){
+			total_obstacle += (cpt_obstacle(board[k+i-1][j]) - cpt_obstacle(board[k-1][j]));
+			//printf("board[%d][%d]=%d,board[%d][%d]=%d\n",k+i-1,j,cpt_obstacle(board[k+i-1][j] ,k-1,j,cpt_obstacle(board[k-1][j]);
+			obstacle[k][j] = total_obstacle;
+		}//obstacle[0][*] = 0
+	}
 }
